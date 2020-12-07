92c45376045832edd8e86c1a9bbffac5fcb88fed
Karl Lattimer <karl@qdh.org.uk>
2020-12-07 23:01:45 +0000

    .

--- energenie/Handlers/__init__.py
+++ energenie/Handlers/__init__.py
@@ -107,6 +107,7 @@ class HandlerRegistry:
 
     @classmethod
     def handle_reading(cls, device, key, value):
+        logging.debug("Handling message 1")
         cls.singleton()._handle_reading(device, key, value)
 
     def __init__(self):
@@ -121,5 +122,7 @@ class HandlerRegistry:
             self._handlers[handler_args['name']](handler)
 
     def _handle_reading(self, device, key, value):
+        logging.debug("Handling message 2")
         for handler in self._handlers.values():
+            logging.debug("Handling message 3")
             handler.handle_reading(device, key, value)
