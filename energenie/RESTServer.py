4c2a705eead40f96a455392c2dfa1ff2b5da8181
Karl Lattimer <karl@qdh.org.uk>
2020-12-10 10:57:09 +0000

    .

--- energenie/RESTServer.py
+++ energenie/RESTServer.py
@@ -62,5 +62,6 @@ def handlers():
 def start():
     def run():
         app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
-    thread = threading.Thread(target=run).start()
+    thread = threading.Thread(target=run)
+    thread.start()
     return thread
