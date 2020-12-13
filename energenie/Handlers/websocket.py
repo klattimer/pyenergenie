b9278363cb4cdc011b6da5a9c787fd12f6451b13
Karl Lattimer <karl@qdh.org.uk>
2020-12-13 22:56:15 +0000

    cleanup websocket server

--- energenie/Handlers/websocket.py
+++ energenie/Handlers/websocket.py
@@ -24,7 +24,7 @@ class WebSocketHandler(Handler):
         self.port = kw_args.get('port')
         self._clients = set()
 
-        start_server = websockets.serve(self.server, "localhost", 8765)
+        start_server = websockets.serve(self.server, "localhost", self.port)
 
         asyncio.get_event_loop().run_until_complete(start_server)
         asyncio.get_event_loop().run_forever()
@@ -38,38 +38,26 @@ class WebSocketHandler(Handler):
     async def server(self, websocket, path):
         await self.register(websocket)
         try:
-            # await websocket.send(state_event())
             async for message in websocket:
                 self.handle_message(message)
-                # data = json.loads(message)
-                # if data["action"] == "minus":
-                #     STATE["value"] -= 1
-                #     await notify_state()
-                # elif data["action"] == "plus":
-                #     STATE["value"] += 1
-                #     await notify_state()
-                # else:
-                #     logging.error("unsupported event: {}", data)
         finally:
             await self.unregister(websocket)
-        #
-        # name = await websocket.recv()
-        # print(f"< {name}")
-        #
-        # greeting = f"Hello {name}!"
-        #
-        # await websocket.send(greeting)
-        # print(f"> {greeting}")
 
     async def handle_message(self, message):
-        pass
+        device = message['uuid']
+        device = self.registry.get(device)
+        for key, value in message['states']:
+            device.do_set_state(key, value)
 
     async def handle_reading(self, device, key, value):
-        device = self.registry.get(device)
-        message = {
-
-        }
         if len(self._clients) == 0:
             return
+        device = self.registry.get(device)
+        message = {
+            'uuid': device.uuid,
+            'states': {
+                key: value
+            }
+        }
 
         await asyncio.wait([client.send(json.dumps(message)) for client in self._clients])
